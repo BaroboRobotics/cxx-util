04bf74b44621601a2307785cdbe5ebbac47339c7
Harris Hancock <vortrab@gmail.com>
2015-02-10 18:26:49 -0800

    Add LogSafely operator for local_clock

--- include/util/logsafely.hpp
+++ include/util/logsafely.hpp
@@ -1,6 +1,7 @@
 #ifndef UTIL_LOGSAFELY_HPP
 #define UTIL_LOGSAFELY_HPP
 
+#include <boost/log/attributes/clock.hpp>
 #include <boost/log/attributes/timer.hpp>
 #include <boost/log/utility/formatting_ostream.hpp>
 #include <boost/log/utility/manipulators/to_log.hpp>
@@ -9,14 +10,19 @@
 
 namespace util {
 
-struct LogSafely;
+// Boost.Log uses Boost.DateTime's ptime class for clock and timer values.
+// DateTime uses setlocale(), which is not thread-safe under Windows XP. To
+// avoid setlocale() related crashes, we provide a pair of safe operator<<
+// overloads for formatting Boost.Log clocks and timers according to ISO 8601.
 
-// The default formatter for boost::log::timer::value_type uses setlocale(),
-// which is not thread-safe on Windows XP. Boost.Log provides a way to select
-// a different operator<< for log record formatting, which we can use to work
-// around the problem. To select this overload, our formatter would use the
-// form: expr::attr<attrs::timer::value_type, LogSafely>("Timeline").
+// To select these overloads, our formatter would use the form:
+//   expr::attr<attrs::timer::value_type, LogSafely>("Timeline")
+// or
+//   expr::attr<attrs::clock::value_type, LogSafely>("TimeStamp")
+
+struct LogSafely;
 using FormatStream = boost::log::formatting_ostream;
+
 using TimerValueType = boost::log::attributes::timer::value_type;
 using SafeTimerToLogManip = boost::log::to_log_manip<TimerValueType, LogSafely>;
 
@@ -28,6 +34,19 @@ FormatStream& operator<< (FormatStream& os, const SafeTimerToLogManip& manip) {
        << std::setw(2) << t.seconds() << "."
        << std::setw(6) << t.fractional_seconds();
     return os;
+}
+
+using LocalClockValueType = boost::log::attributes::local_clock::value_type;
+using SafeLocalClockToLogManip = boost::log::to_log_manip<LocalClockValueType, LogSafely>;
+
+FormatStream& operator<< (FormatStream& os, const SafeLocalClockToLogManip& manip) {
+	auto& t = manip.get();
+	os << std::setfill('0')
+	   << std::setw(4) << t.date().year() << '-'
+	   << std::setw(2) << int(t.date().month()) << '-'
+	   << std::setw(2) << t.date().day() << 'T'
+	   << SafeTimerToLogManip{t.time_of_day()};
+   return os;
 }
 
 } // namespace util
