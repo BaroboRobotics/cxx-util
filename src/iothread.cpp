703078812ceb6aa20abe935a6ec59703da53bdea
Harris Hancock <harris@barobo.com>
2016-01-06 22:09:36 -0800

    Migrate baromesh::IoCore to util::IoThread

---
+++ src/iothread.cpp
@@ -0,0 +1,44 @@
+#include <util/iothread.hpp>
+
+#include <boost/utility/in_place_factory.hpp>
+
+#include <exception>
+#include <mutex>
+#include <string>
+
+#include <cstdlib>
+
+namespace util {
+
+IoThread::IoThread ()
+    : mWork(boost::in_place(std::ref(mContext)))
+    // Run the io_service in a separate thread
+    , mJoin(std::async(std::launch::async, [this] () { return mContext.run(); }))
+{
+}
+
+IoThread::~IoThread () {
+    // If this throws, you have a bug in your program. Call join() before the
+    // destructor, catch the exception, and figure out what's wrong.
+    (void)join();
+}
+
+size_t IoThread::join () {
+    mWork = boost::none;
+    return mJoin.valid() ? mJoin.get() : 0;
+}
+
+std::shared_ptr<IoThread> IoThread::getGlobal () {
+    static std::mutex mutex;
+    std::lock_guard<std::mutex> lock {mutex};
+
+    static auto wp = std::weak_ptr<IoThread>{};
+    auto p = wp.lock();
+    if (!p) {
+        p = std::make_shared<IoThread>();
+        wp = p;
+    }
+    return p;
+}
+
+} // namespace util
