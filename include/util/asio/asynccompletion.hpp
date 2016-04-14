2154b53e9a5c648332cd1fa0f7d1ece33f9780ea
Harris Hancock <harris@barobo.com>
2016-04-14 12:29:24 -0700

    Simplify util::AsyncCompletion for great good

--- include/util/asio/asynccompletion.hpp
+++ include/util/asio/asynccompletion.hpp
@@ -10,23 +10,13 @@ namespace util {
 // This is basically just async_result_init/async_completion from Asio.
 template <class CompletionToken, class Signature>
 struct AsyncCompletion {
-    // The real handler type to be used for the asynchronous operation.
-    using HandlerType = typename boost::asio::handler_type<CompletionToken, Signature>::type;
-
-    template <class If, class Else>
-    using IfTokenIsHandler
-        = typename std::conditional<
-            std::is_same<CompletionToken, HandlerType>::value, If, Else
-        >::type;
-
-    explicit AsyncCompletion (typename std::remove_reference<CompletionToken>::type& token)
-        : handler(static_cast<IfTokenIsHandler<HandlerType&, CompletionToken&&>>(token))
+    explicit AsyncCompletion (CompletionToken&& token)
+        : handler(std::move(token))
         , result(handler) {}
 
-    // A copy of, or reference to, a real handler object.
-    IfTokenIsHandler<HandlerType&, HandlerType> handler;
+    using HandlerType = typename boost::asio::handler_type<CompletionToken, Signature>::type;
 
-    // The result of the asynchronous operation's initiating function.
+    HandlerType handler;
     boost::asio::async_result<HandlerType> result;
 };
 
