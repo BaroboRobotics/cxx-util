d87a536b8d5f4e4d0d8f146be54e33d41515e34c
Harris Hancock <harris@barobo.com>
2016-12-02 14:18:21 -0800

    Steal overload and overload_linearly from Boost.Hana

--- include/util/overload.hpp
+++ include/util/overload.hpp
@@ -1,44 +1,136 @@
-// Copyright (c) 2015-2016 Vittorio Romeo
-// License: Academic Free License ("AFL") v. 3.0
-// AFL License page: http://opensource.org/licenses/AFL-3.0
-// http://vittorioromeo.info | vittorio.romeo@outlook.com
+// Copyright Louis Dionne 2013-2016
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 //
-// Barobo-styled by Harris Hancock.
+// Stolen from Boost.Hana, Barobo-ified, and hacked to work with gcc 4.9 by Harris Hancock
 
+#include <type_traits>
 #include <utility>
 
 namespace util { namespace _ {
 
-template <class...>
-struct OverloadSet;
+template <typename F, typename ...G>
+struct overload_t
+    : overload_t<F>::type
+    , overload_t<G...>::type
+{
+    using type = overload_t;
+    using overload_t<F>::type::operator();
+    using overload_t<G...>::type::operator();
 
-template <class F>
-struct OverloadSet<F>: F {
-    using CallType = F;
-    using CallType::operator();
-
-    OverloadSet(F&& f) noexcept: F(std::forward<decltype(f)>(f)) {}
+    template <typename F_, typename ...G_>
+    constexpr explicit overload_t(F_&& f, G_&& ...g)
+        : overload_t<F>::type(static_cast<F_&&>(f))
+        , overload_t<G...>::type(static_cast<G_&&>(g)...)
+    { }
 };
 
-template <class F, class... Fs>
-struct OverloadSet<F, Fs...>: F, OverloadSet<Fs...>::CallType {
-    using BaseType = typename OverloadSet<Fs...>::CallType;
+template <typename F>
+struct overload_t<F> { using type = F; };
 
-    using FType = F;
-    using CallType = OverloadSet;
+template <typename R, typename ...Args>
+struct overload_t<R(*)(Args...)> {
+    using type = overload_t;
+    R (*fptr_)(Args...);
 
-    OverloadSet(F&& f, Fs&&... fs) noexcept: FType(std::forward<F>(f)), BaseType(std::forward<Fs>(fs)...) {}
+    explicit constexpr overload_t(R (*fp)(Args...))
+        : fptr_(fp)
+    { }
 
-    using FType::operator();
-    using BaseType::operator();
+    constexpr R operator()(Args ...args) const
+    { return fptr_(static_cast<Args&&>(args)...); }
+};
+
+struct make_overload_t {
+    template <typename ...F,
+        typename Overload = typename overload_t<
+            typename std::decay<F>::type...
+        >::type
+    >
+    constexpr Overload operator()(F&& ...f) const {
+        return Overload(static_cast<F&&>(f)...);
+    }
 };
 
 }  // _
 
-template <class... Fs>
-auto overload(Fs&&... fs) noexcept {
-    // Merge all given function objects `fs` into a single function object with many overloads.
-    return _::OverloadSet<Fs...>{std::forward<Fs>(fs)...};
-}
+constexpr _::make_overload_t overload{};
+
+namespace _ {
+
+template <typename F, typename G>
+struct overload_linearly_t {
+    F f;
+    G g;
+
+private:
+    template <typename ...Args, typename =
+        decltype(std::declval<F const&>()(std::declval<Args>()...))>
+    constexpr F const& which(int) const& { return f; }
+
+#if 0
+    // gcc 4.9 seems to have trouble with ref-qualified overloads
+
+    template <typename ...Args, typename =
+        decltype(std::declval<F&>()(std::declval<Args>()...))>
+    constexpr F& which(int) & { return f; }
+
+    template <typename ...Args, typename =
+        decltype(std::declval<F&&>()(std::declval<Args>()...))>
+    constexpr F which(int) && { return static_cast<F&&>(f); }
+#endif
+
+    template <typename ...Args>
+    constexpr G const& which(long) const& { return g; }
+
+#if 0
+    template <typename ...Args>
+    constexpr G& which(long) & { return g; }
+
+    template <typename ...Args>
+    constexpr G which(long) && { return static_cast<G&&>(g); }
+#endif
+
+public:
+    template <typename ...Args>
+    constexpr decltype(auto) operator()(Args&& ...args) const&
+    { return which<Args...>(int{})(static_cast<Args&&>(args)...); }
+
+#if 0
+// gcc 4.9 doesn't like this
+    template <typename ...Args>
+    constexpr decltype(auto) operator()(Args&& ...args) &
+    { return which<Args...>(int{})(static_cast<Args&&>(args)...); }
+
+    template <typename ...Args>
+    constexpr decltype(auto) operator()(Args&& ...args) &&
+    { return which<Args...>(int{})(static_cast<Args&&>(args)...); }
+#endif
+};
+
+struct make_overload_linearly_t {
+    template <typename F, typename G>
+    constexpr overload_linearly_t<
+        typename std::decay<F>::type,
+        typename std::decay<G>::type
+    > operator()(F&& f, G&& g) const {
+        return {static_cast<F&&>(f), static_cast<G&&>(g)};
+    }
+
+    template <typename F, typename G, typename ...H>
+    constexpr decltype(auto) operator()(F&& f, G&& g, H&& ...h) const {
+        return (*this)(static_cast<F&&>(f),
+                (*this)(static_cast<G&&>(g), static_cast<H&&>(h)...));
+    }
+
+    template <typename F>
+    constexpr typename std::decay<F>::type operator()(F&& f) const {
+        return static_cast<F&&>(f);
+    }
+};
+
+}  // _
+
+constexpr _::make_overload_linearly_t overloadLinearly{};
 
 }  // util
