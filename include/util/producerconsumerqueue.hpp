202ddbabf24437f370e3acb710265ac57462db6d
Harris Hancock <harris@barobo.com>
2017-01-19 18:47:08 -0800

    Clean up pc-queue comments

--- include/util/producerconsumerqueue.hpp
+++ include/util/producerconsumerqueue.hpp
@@ -15,36 +15,34 @@
 
 namespace util {
 
-// A queue supporting three operations, consume, produce, and clear.
-//   consume: enqueue a function object to be invoked when data are produced
-//   produce: enqueue data to be called as arguments to a pulling function object
-//   clear: clear the consuming function object queue by invoking all function objects in excess of
-//     the result queue with a set of default parameters; clear the result queue by invoking all
-//     data in excess of the function object queue on a default function object
-//
-// If consume is called before any data are in the buffer, the function object is saved for later
-// invocation. If consume is called when data are in the buffer, the function object is
-// immediately invoked.
-//
-// If produce is called before any consuming function objects are in the buffer, the data are saved
-// for later invocation on a future function object. If produce is called when a consuming function
-// object is waiting, that function object is immediately invoked.
 template <class... Data>
 class ProducerConsumerQueue {
 public:
     template <class H>
     void consume (H&& handler) {
+        // Enqueue a pulling function object to be invoked when data are produced. If consume is
+        // called before any data are in the buffer, the function object is saved for later
+        // invocation. If consume is called when data are in the buffer, the function object is
+        // immediately invoked.
         mHandlers.emplace(std::forward<H>(handler));
         post();
     }
 
     template <class... Ds>
     void produce (Ds&&... data) {
+        // Enqueue data to be called as arguments to a pulling function object. If produce is called
+        // before any consuming function objects are in the buffer, the data are saved for later
+        // invocation on a future function object. If produce is called when a consuming function
+        // object is waiting, that function object is immediately invoked.
         mData.emplace(std::make_tuple(std::forward<Ds>(data)...));
         post();
     }
 
     int depth () const {
+        // Depth is:
+        //    0   if the queue is empty
+        //    > 0 if the queue has supply (surplus data)
+        //    < 0 if the queue has demand (surplus consumers)
         return mData.size() - mHandlers.size();
     }
 
@@ -65,16 +63,6 @@ private:
     std::queue<Handler> mHandlers;
     std::queue<DataTuple> mData;
 };
-
-template <class... Data>
-bool hasSupply (const ProducerConsumerQueue<Data...>& q) {
-    return q.depth() < 0;
-}
-
-template <class... Data>
-bool hasDemand (const ProducerConsumerQueue<Data...>& q) {
-    return q.depth() > 0;
-}
 
 } // namespace util
 
